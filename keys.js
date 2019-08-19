001cfc25b9258f55f1626cb1e7a73803aa490548
joshroy326 <joshroy326@gmail.com>
2019-08-18 19:52:43 -0500

    pushing latest info and updating

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 console.log('this is loaded');
 
 exports.spotify = {
-  id: process.env.c0c2a81968f34f1bbb57ff50668781cd,
-  secret: process.env.bd087cf78c004d80bc204927c947626c
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
 };
